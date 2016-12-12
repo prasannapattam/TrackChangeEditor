c91d40d3d68bc79c4d54d1b8f2f6fe20bf6ccc5d
Prasanna Kumar Pattam <pprasannak@hotmail.com>
2016-12-12 22:03:07 +0530

    POC to show the capability of change tracker in WYSIWYG editor

---
+++ Editor/src/Editor/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Editor.Models;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Editor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult SelectUser()
+        {
+            EditorModel model = new EditorModel();
+            string editorString = HttpContext.Session.GetString("currenteditor");
+            if (!String.IsNullOrEmpty(editorString))
+            {
+                model = JsonConvert.DeserializeObject<EditorModel>(editorString);
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult SelectUser(EditorModel model)
+        {
+            EditorModel newModel = new EditorModel();
+            newModel.UserName = model.UserName;
+
+            string editorString = HttpContext.Session.GetString("currenteditor");
+            if (!String.IsNullOrEmpty(editorString))
+            {
+                model = JsonConvert.DeserializeObject<EditorModel>(editorString);
+                newModel.Text = model.Text;
+            }
+
+            HttpContext.Session.SetString("currenteditor", JsonConvert.SerializeObject(newModel));
+            return RedirectToAction("Editor");
+        }
+
+        public IActionResult Editor()
+        {
+            string editorString = HttpContext.Session.GetString("currenteditor");
+            EditorModel model = JsonConvert.DeserializeObject<EditorModel>(editorString);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Editor(string editor1)
+        {
+            string editorString = HttpContext.Session.GetString("currenteditor");
+            EditorModel model = JsonConvert.DeserializeObject<EditorModel>(editorString);
+            model.Text = editor1;
+            HttpContext.Session.SetString("currenteditor", JsonConvert.SerializeObject(model));
+            return RedirectToAction("SelectUser");
+        }
+    }
+}
---
+++ Editor/src/Editor/Models/EditorModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Editor.Models
+{
+    public class EditorModel
+    {
+        public string Text { get; set; } = @"&lt;h1&gt;&lt;img alt=""Saturn V carrying Apollo 11"" class=""right"" src=""http://sdk.ckeditor.com/samples/assets/sample.jpg""/&gt; Apollo 11&lt;/h1&gt;		&lt;p&gt;&lt;strong&gt;Apollo 11&lt;/strong&gt; was the spaceflight that landed the first humans, Americans &lt;a href=""http://en.wikipedia.org/wiki/Neil_Armstrong""&gt;Neil		Armstrong&lt;/a&gt; and		&lt;a href=""http://en.wikipedia.org/wiki/Buzz_Aldrin""&gt;Buzz Aldrin&lt;/a&gt;, on the Moon on July 20, 1969, at 20:18 UTC. Armstrong became the first to step onto the		lunar surface 6 hours		later on July 21 at 02:56 UTC.&lt;/p&gt;		&lt;p&gt;Armstrong spent about &lt;s&gt;three and a half&lt;/s&gt; two and a half hours outside the spacecraft, Aldrin slightly less; and together they collected 47.5 pounds (21.5&amp;nbsp;kg)		of lunar		material for return to Earth. A third member of the mission, &lt;a href=""http://en.wikipedia.org/wiki/Michael_Collins_(astronaut)""&gt;Michael Collins&lt;/a&gt;, piloted		the		&lt;a href=""http://en.wikipedia.org/wiki/Apollo_Command/Service_Module""&gt;command&lt;/a&gt; spacecraft alone in lunar orbit until Armstrong and Aldrin returned to it for		the trip back to		Earth.&lt;/p&gt;";
+        public string UserName { get; set; } = "Prasanna";
+    }
+}
